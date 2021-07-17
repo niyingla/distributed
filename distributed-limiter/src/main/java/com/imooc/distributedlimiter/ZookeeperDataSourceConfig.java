a8facf0d31fa0be34ec6bb0c7355574d3c5a7cd2
火火 <1047984367@qq.com>
2021-07-18 03:13:32 +0800

    增加备注

--- distributed-limiter/src/main/java/com/imooc/distributedlimiter/ZookeeperDataSourceConfig.java
+++ distributed-limiter/src/main/java/com/imooc/distributedlimiter/ZookeeperDataSourceConfig.java
@@ -1,9 +1,11 @@
 package com.imooc.distributedlimiter;
 
 import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
-import com.alibaba.csp.sentinel.datasource.zookeeper.ZookeeperDataSource;
+import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRuleManager;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import org.springframework.context.annotation.Bean;
@@ -24,9 +26,14 @@ public class ZookeeperDataSourceConfig {
         //存储规则地址
         final String path = "/limiter/sentinel-flow-rules";
 
-        //指定流控规则存储数据源
-        ReadableDataSource<String, List<FlowRule>> flowRuleDataSource = new ZookeeperDataSource<>(remoteAddress, path, source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {}));
-        //写入流控管理
-        FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
+//        //指定流控规则存储数据源
+//        ReadableDataSource<String, List<FlowRule>> flowRuleDataSource = new Nac<>(remoteAddress, path, source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {}));
+//        //写入流控管理
+//        FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
+
+        //指定nacos 读取规则
+        ReadableDataSource<String, List<ParamFlowRule>> paramRuleSource = new NacosDataSource<>(remoteAddress, "groupId",
+                "paramDataId", source -> JSON.parseObject(source, new TypeReference<List<ParamFlowRule>>() {}));
+        ParamFlowRuleManager.register2Property(paramRuleSource.getProperty());
     }
 }
